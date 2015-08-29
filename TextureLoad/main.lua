0cf7649958fcbb545e007e17c42dd63e699e43a7
Vlad Shcherban <vlad@coronalabs.com>
2015-08-28 23:19:27 -0400

    Default gihub mirror

--- TextureLoad/main.lua
+++ TextureLoad/main.lua
@@ -9,7 +9,7 @@ display.setStatusBar( display.HiddenStatusBar )
 
 
 composer.setVariable( 'dlcUrl', 'https://raw.githubusercontent.com/Shchvova/test/master/dlc/' )
-composer.setVariable( 'dlcUrl', 'http://localhost/dcl/dlc/' )
+-- composer.setVariable( 'dlcUrl', 'http://localhost/dcl/dlc/' )
 
 
 composer.gotoScene( 'loadingScreen' )
